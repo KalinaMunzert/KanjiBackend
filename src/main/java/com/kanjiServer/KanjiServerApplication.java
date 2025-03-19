83c70ad18279c4af31b371309a01bf88e2613288
KalinaMunzert <munzert.kalina@gmail.com>
2025-03-19 08:57:05 -0600

    checking test branch

--- src/main/java/com/kanjiServer/KanjiServerApplication.java
+++ src/main/java/com/kanjiServer/KanjiServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KanjiServerApplication {
 
-	// AS OF 3/18 01:30, I got the frontend and backend connected. The timer is null though. 
-
+	// AS OF 3/18 01:30, I got the frontend and backend connected. The timer is null though.
+	// comment
 	public static void main(String[] args) {
 		SpringApplication.run(KanjiServerApplication.class, args);
 	}
